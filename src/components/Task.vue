b37ffffaa23336f101ed5df0ab673f69b0efcb1d
bpinazmul18 <bpinazmul18@gmail.com>
2021-06-01 17:47:35 +0600

    add toggleReminder

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,11 @@
 <template>
-  <div :class="[task.reminder ? 'reminder' : '', 'task']">
+  <div
+    @dblclick="$emit('toggle-reminder', task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task']"
+  >
     <h3>
       {{ task.text }}
-      <i @click="onDelete(task.id)" class="fas fa-times"></i>
+      <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
     </h3>
     <p>{{ task.day }}</p>
   </div>
@@ -13,11 +16,6 @@ export default {
   name: "Task",
   props: {
     task: Object,
-  },
-  methods: {
-    onDelete(id) {
-      this.$emit("delete-task", id);
-    },
   },
 };
 </script>
@@ -34,7 +32,7 @@ export default {
   background: #f4f4f4;
   margin: 5px;
   padding: 10px 20px;
-  /*cursor: pointer;*/
+  cursor: pointer;
 }
 .task.reminder {
   border-left: 5px solid green;
