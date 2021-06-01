5a1ac3fcc3f3d2290804a87f44b29500ed0abbe4
bpinazmul18 <bpinazmul18@gmail.com>
2021-06-01 11:32:59 -0700

    add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    v-on:click="addTaskHandler"
+    :style="{ background: color }"
+    class="btn"
+  >
+    Add Task
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    Button: {
+      text: String,
+    },
+    color: String,
+  },
+  methods: {
+    addTaskHandler(e) {
+      console.log("you click me!", e);
+    },
+  },
+};
+</script>
